77ec3e1ae0c1e813044a741b44253f7490bb12cd
Coolchh <1053968489@qq.com>
2018-11-27 08:53:20 +0800

    汪德意

---
+++ LYY_SnacksWeb/Controllers/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace LYY_SnacksWeb.Controllers
+{
+    public class Class1
+    {
+        int a = 6;
+    }
+}
